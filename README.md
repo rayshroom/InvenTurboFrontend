acdf66c47dc1a5ef36b44dd5976e7c458c54926c
rayshroom <goodcolorpencils@gmail.com>
2019-09-19 20:57:01 -0400

    borrowed part from a MIT licensed project, for showcase only, refactor later

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+`auth.service.ts` contains the main functionalities for login with Firebase  
+
+i.e. `firebase.auth.FacebookAuthProvider`  
+
+Sign in returns a promise
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAJT1qKhwTCrjd_uWC4t4UWJoXf6S_dK2w",
+    authDomain: "inventurbo-f0eca.firebaseapp.com",
+    databaseURL: "https://inventurbo-f0eca.firebaseio.com",
+    projectId: "inventurbo-f0eca",
+    storageBucket: "inventurbo-f0eca.appspot.com",
+    messagingSenderId: "216500507265",
+    appId: "1:216500507265:web:d8d15c63b08b0ecd55f380"
+  }
+};
