1a24b9e68915f582f9688be052ec83895fb47c9c
VictorHadziristic <victor.hadziristic@gmail.com>
2019-12-06 01:03:56 -0500

    Here goes nothing

--- src/app/components/views/inventory-page/inventory-page.component.ts
+++ src/app/components/views/inventory-page/inventory-page.component.ts
@@ -4,6 +4,7 @@ import { TransactionManagementService } from 'src/app/services/transaction/trans
 import { UserOrganization } from 'src/app/services/organization/user-organization.model';
 import { UserOrganizationService } from 'src/app/services/organization/user-organization.service';
 import { Url } from 'url';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-inventory-page',
@@ -13,15 +14,71 @@ import { Url } from 'url';
 export class InventoryPageComponent implements OnInit {
   user: firebase.User;
   org: UserOrganization;
-  transactions: {pid: string, quantity: number, imageURL: Url}[];
-  constructor(
-    public auth: AuthService,
-    public userOrg: UserOrganizationService,
-    public m: TransactionManagementService
-  ) { 
+  products: {pid: string, cols:number, rows:number, quantity: number, productPhoto: string}[];
 
-  }
+    getTransactionItemBackgroundCss(color) {
+        const col = color ? color : 'rgb(255,255,255)' ;
+        return  {
+            background: `linear-gradient(90deg, ${col} 12.5%, rgba(255,255,255,0) 12.5%)`,
+            'padding-left': '3%'
+        };
+    }
 
-  ngOnInit() {
-  }
+    constructor(
+        public auth: AuthService,
+        public userOrg: UserOrganizationService,
+        public m: TransactionManagementService,
+    ) {
+        this.products = [
+            {
+              pid: '00001',
+              cols: 1,
+              rows: 1,
+              quantity: 40,
+              productPhoto: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTyP9fd4p1hd9kPKEiCfdiiXhzY-h2zr96PrlstJiUKeR6c5YZIk8GyBvbTfCkYt7sHTEnI6XU8RA&usqp=CAc"
+            },
+            {
+              pid: '00001',
+              cols: 2,
+              rows: 1,
+              quantity: 40,
+              productPhoto: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTyP9fd4p1hd9kPKEiCfdiiXhzY-h2zr96PrlstJiUKeR6c5YZIk8GyBvbTfCkYt7sHTEnI6XU8RA&usqp=CAc"
+            },
+            {
+              pid: '00001',
+              cols: 3,
+              rows: 1,
+              quantity: 40,
+              productPhoto: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTyP9fd4p1hd9kPKEiCfdiiXhzY-h2zr96PrlstJiUKeR6c5YZIk8GyBvbTfCkYt7sHTEnI6XU8RA&usqp=CAc"
+            },
+            {
+              pid: '00001',
+              cols: 4,
+              rows: 1,
+              quantity: 40,
+              productPhoto: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTyP9fd4p1hd9kPKEiCfdiiXhzY-h2zr96PrlstJiUKeR6c5YZIk8GyBvbTfCkYt7sHTEnI6XU8RA&usqp=CAc"
+            },
+            {
+              pid: '00001',
+              cols: 5,
+              rows: 1,
+              quantity: 40,
+              productPhoto: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTyP9fd4p1hd9kPKEiCfdiiXhzY-h2zr96PrlstJiUKeR6c5YZIk8GyBvbTfCkYt7sHTEnI6XU8RA&usqp=CAc"
+            },
+            {
+              pid: '00001',
+              cols: 6,
+              rows: 1,
+              quantity: 40,
+              productPhoto: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTyP9fd4p1hd9kPKEiCfdiiXhzY-h2zr96PrlstJiUKeR6c5YZIk8GyBvbTfCkYt7sHTEnI6XU8RA&usqp=CAc"
+            }
+        ];
+    }
+
+    ngOnInit() {
+    }
+
+    public myfunction(){
+      
+    }
 }
