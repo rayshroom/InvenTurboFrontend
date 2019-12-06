dd6620b5a66dfc907725f8235fdf41c36bf4bf99
VictorHadziristic <victor.hadziristic@gmail.com>
2019-12-05 20:18:01 -0500

    Initial Setup

--- src/app/components/views/inventory-page/inventory-page.component.ts
+++ src/app/components/views/inventory-page/inventory-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { TransactionManagementService } from 'src/app/services/transaction/transaction-management.service';
+import { UserOrganization } from 'src/app/services/organization/user-organization.model';
+import { UserOrganizationService } from 'src/app/services/organization/user-organization.service';
+import { Url } from 'url';
 
 @Component({
   selector: 'app-inventory-page',
   templateUrl: './inventory-page.component.html',
-  styleUrls: ['./inventory-page.component.sass']
+  styleUrls: ['./inventory-page.component.scss']
 })
 export class InventoryPageComponent implements OnInit {
+  user: firebase.User;
+  org: UserOrganization;
+  transactions: {pid: string, quantity: number, imageURL: Url}[];
+  constructor(
+    public auth: AuthService,
+    public userOrg: UserOrganizationService,
+    public m: TransactionManagementService
+  ) { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
-
 }
