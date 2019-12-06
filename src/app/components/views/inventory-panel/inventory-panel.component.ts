43f4a294328c9b7d3354b99816389371ed249346
Khang Nguyen <khangngocnamnguyen@outlook.com>
2019-12-06 07:08:13 -0500

    Add real-time search bar for inventory view

--- src/app/components/views/inventory-panel/inventory-panel.component.ts
+++ src/app/components/views/inventory-panel/inventory-panel.component.ts
@@ -4,6 +4,7 @@ import { UserOrganizationService } from 'src/app/services/organization/user-orga
 import { ProductStock } from 'src/app/services/product/product-stock.model';
 import { ProductStockService } from 'src/app/services/product/product-stock.service';
 import { Location } from '@angular/common';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-inventory-panel',
@@ -11,21 +12,41 @@ import { Location } from '@angular/common';
     styleUrls: ['./inventory-panel.component.scss']
 })
 export class InventoryPanelComponent implements OnInit {
+    public search: FormGroup;
+
     public org: UserOrganization;
     public products: ProductStock[];
+    public filteredProducts: ProductStock[];
 
     constructor(
+        private fb: FormBuilder,
         private loc: Location,
         public userOrg: UserOrganizationService,
         public prodStock: ProductStockService,
     ) {
+        this.search = this.fb.group({
+            searchBar: ['']
+        });
         this.org = this.userOrg.getCurrentOrganization();
         if (!this.org.photoURL) {
             this.org.photoURL = 'assets/default-org-avatar.png';
         }
         this.prodStock.getAllOrganizationProductStock(this.org.oid).subscribe(prod => {
             this.products = prod;
+            this.filteredProducts = prod;
         });
+    }
+
+    filterItems() {
+        const trimmed = this.search.get('searchBar').value.trim();
+        if (trimmed !== '') {
+            this.filteredProducts = this.products.filter(prod =>
+                prod.name.includes(trimmed) ||
+                prod.description.includes(trimmed)
+            );
+        } else {
+            this.filteredProducts = this.products;
+        }
     }
 
     goBack() {
