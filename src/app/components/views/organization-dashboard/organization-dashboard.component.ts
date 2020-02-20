2acc5c64c48ceac5c0d3a908b96e666883daf950
Rafi Ungar <raungar@gmail.com>
2020-02-19 21:40:18 -0500

    initial work on filters

--- src/app/components/views/organization-dashboard/organization-dashboard.component.ts
+++ src/app/components/views/organization-dashboard/organization-dashboard.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { TransactionManagementService } from 'src/app/services/transaction/transaction-management.service';
 import { UserOrganization } from 'src/app/services/organization/user-organization.model';
 import { UserOrganizationService } from 'src/app/services/organization/user-organization.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-organization-dashboard-view',
@@ -13,8 +14,94 @@ import { UserOrganizationService } from 'src/app/services/organization/user-orga
 export class OrganizationDashboardComponent implements OnInit {
     user: firebase.User;
     org: UserOrganization;
-    transactions: {tid: string, status: string, timeDate: Date, transactionType: string, oid_source: string, oid_dest: string}[];
+    transactions: any = [];
     lastTransactionTime: Date;
+    filters = [
+        {
+            id: 'all',
+            label: 'All Transactions'
+        },
+        {
+            id: 'my',
+            label: 'My Transactions'
+        },
+        {
+            id: 'range',
+            label: 'Transactions within a date range...'
+        },
+        {
+            id: 'pending',
+            label: 'Pending Transactions'
+        },
+        {
+            id: 'initiated',
+            label: 'Initiated Transactions'
+        },
+        {
+            id: 'approved',
+            label: 'Approved Transactions'
+        },
+        {
+            id: 'shipped',
+            label: 'Shipped Transactions'
+        },
+        {
+            id: 'completed',
+            label: 'Completed Transactions'
+        }
+
+    ]
+    filterType: string = this.filters[0].id;
+    filterDateStart: any;
+    filterDateEnd: any;
+
+    shouldDisplay(transaction) {
+        this.filterDateStart = this.filterDateEnd = null;
+
+        if (this.filterType == 'all') {
+            return true;
+        }
+        else if (this.filterType == 'my') {
+            if (transaction.currentUser)
+                return transaction.currentUser.displayName == this.user.displayName;
+        }
+        else if (this.filterType == 'range') {
+            if (!this.filterDateStart || !this.filterDateEnd) {
+                return true;
+            }
+            else {
+                var s, e;
+                s = new Date(this.filterDateStart)
+                e = new Date(this.filterDateEnd)
+                console.log(Date.now() >= s && Date.now() <= e)
+                if (Date.now() >= s && Date.now() <= e) {
+                    return true;
+                }
+            }
+        }
+        else if (this.filterType == 'pending') {
+            return transaction.status.toLowerCase() == 'pending';
+        }
+        else if (this.filterType == 'initiated') {
+            return transaction.status.toLowerCase() == 'initiated';
+        }
+        else if (this.filterType == 'approved') {
+            return transaction.status.toLowerCase() == 'approved';
+        }
+        else if (this.filterType == 'shipped') {
+            return transaction.status.toLowerCase() == 'shipped';
+        }
+        else if (this.filterType == 'completed') {
+            return transaction.status.toLowerCase() == 'completed';
+        }
+
+        return false;
+    }
+
+
+    getProfileLink(uid) {
+        return `${environment.api}${environment.routes.getOneProfile(uid)}`;
+    }
 
     getTransactionItemBackgroundCss(color) {
         const col = color ? color : 'rgb(255,255,255)' ;
@@ -26,6 +113,7 @@ export class OrganizationDashboardComponent implements OnInit {
         public auth: AuthService,
         public userOrg: UserOrganizationService,
         public tms: TransactionManagementService,
+        private _changeDetectionRef : ChangeDetectorRef
     ) {
         this.auth.getCurrentUser().subscribe(async user => {
             this.user = user;
@@ -37,7 +125,7 @@ export class OrganizationDashboardComponent implements OnInit {
             }
             this.tms.getAllOrganizationTransactions(this.org.oid).subscribe(transactions => {
                 transactions.forEach(transaction => {
-                    // console.log(transaction);
+                    console.log(transaction);
                     const timestamp = Date.parse(transaction.stringTime);
                     const transactionDate = !isNaN(timestamp) ? new Date(timestamp) : new Date();
 
@@ -53,4 +141,8 @@ export class OrganizationDashboardComponent implements OnInit {
 
     ngOnInit() {
     }
+
+    // ngAfterViewChecked() : void {
+    //     this._changeDetectionRef.detectChanges();
+    // }
 }
