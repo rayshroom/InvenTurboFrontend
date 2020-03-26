b196b9d33e60f346847bfb368dad02994f8a0109
VictorHadziristic <victor.hadziristic@gmail.com>
2020-03-25 21:08:03 -0400

    [Feature] Pricing, Solution and About Pages
    update

---
+++ src/app/components/views/solutions-page/solutions-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solutions-page',
+  templateUrl: './solutions-page.component.html',
+  styleUrls: ['./solutions-page.component.scss']
+})
+export class SolutionsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
